053cbf9331b2f79ea99dcad64eb07ee82893b63e
vincentwang <vincent.wang610@gmail.com>
2022-07-17 15:12:27 +0800

    feat: submit singleton pattern

---
+++ Singleton/index.go
@@ -0,0 +1,22 @@
+package singleton
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Singleton struct{}
+
+var singleton *Singleton
+var once sync.Once
+
+func initInstance() {
+	fmt.Println("init ")
+	singleton = &Singleton{}
+}
+
+func getInstance() *Singleton {
+	once.Do(initInstance)
+
+	return singleton
+}
