00f17aac9a8c99a4afc252e76ce47fd9f6d5c342
vincentwang <vincent.wang610@gmail.com>
2022-07-20 22:31:27 +0800

    feat: prototype pattern

--- Prototype/index.go
+++ Prototype/index.go
@@ -4,8 +4,34 @@ type Cloneable interface {
 	clone() struct{}
 }
 
-type Customers struct{}
+type Customers struct {
+	customerList []string
+}
+
+func NewCustomers() *Customers {
+	return &Customers{
+		customerList: []string{},
+	}
+}
+
+func NewCustomers2(customerList []string) *Customers {
+	return &Customers{
+		customerList: customerList,
+	}
+}
+
+func (c *Customers) LoadDataFromDB() {
+	c.customerList[0] = "Bharat"
+	c.customerList[1] = "Sahdev"
+	c.customerList[2] = "Richi"
+}
+
+func (c *Customers) GetCustomerList() []string {
+	return c.customerList
+}
 
 func (c *Customers) Cloneable() Customers {
+	var cl []string = c.GetCustomerList()
 
+	return *NewCustomers2(cl)
 }
