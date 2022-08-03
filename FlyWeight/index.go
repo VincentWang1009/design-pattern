97b14f769fcf0d27e38bd8e804df3b8fe17daa54
wangjian23 <wangjian23@kuaishou.com>
2022-08-03 15:44:38 +0800

    feat: add flyweight pattern

---
+++ FlyWeight/index.go
@@ -0,0 +1,85 @@
+package flyweight
+
+import "fmt"
+
+type Dress interface {
+	getColor() string
+}
+
+const (
+	TerroristDressType        = "tDress"
+	CounterTerroristDressType = "ctDress"
+)
+
+var dressFactorySingleInstance = &DressFactory{
+	dressMap: make(map[string]Dress),
+}
+
+type DressFactory struct {
+	dressMap map[string]Dress
+}
+
+func (d *DressFactory) getDressByType(dressType string) (Dress, error) {
+	if d.dressMap[dressType] != nil {
+		return d.dressMap[dressType], nil
+	}
+
+	if dressType == TerroristDressType {
+		d.dressMap[dressType] = newTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+
+	if dressType == CounterTerroristDressType {
+		d.dressMap[dressType] = newTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+	return nil, fmt.Errorf("wrong type")
+}
+
+func getDressFactorySingleInstance() *DressFactory {
+	return dressFactorySingleInstance
+}
+
+type TerroristDress struct {
+	color string
+}
+
+func (t *TerroristDress) getColor() string {
+	return t.color
+}
+
+func newTerroristDress() *TerroristDress {
+	return &TerroristDress{color: "red"}
+}
+
+type CounterTerroristDress struct {
+	color string
+}
+
+func (c *CounterTerroristDress) getColor() string {
+	return c.color
+}
+
+func newCounterTerroristDress() *CounterTerroristDress {
+	return &CounterTerroristDress{color: "green"}
+}
+
+type Player struct {
+	dress      Dress
+	playerType string
+	lat        int
+	long       int
+}
+
+func newPlayer(playerType, dressType string) *Player {
+	dress, _ := getDressFactorySingleInstance().getDressByType(dressType)
+	return &Player{
+		playerType: playerType,
+		dress:      dress,
+	}
+}
+
+func (p *Player) newLocation(lat, long int) {
+	p.lat = lat
+	p.long = long
+}
